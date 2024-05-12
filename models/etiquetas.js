24a89230254fc12408543de27fa173b90b72c2fa
engaherr <kikgamboa@gmail.com>
2024-05-12 15:56:58 -0600

    feat(models/migrations): Agregar migración y modelo para etiquetas

---
+++ models/etiquetas.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class etiquetas extends Model {
+    static associate(models) {
+      etiquetas.belongsToMany(models.usuarios, { as: 'categorias', through: 'usuariosetiquetas', foreignKey: 'idEtiqueta'});
+    }
+  }
+  etiquetas.init({
+    idEtiqueta: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    freezeTableName: true,
+    modelName: 'etiquetas'
+  });
+  return etiquetas;
+};
