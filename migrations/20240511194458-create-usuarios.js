7980944dfaa2872c2193b725ae3981f3d4defb65
engaherr <kikgamboa@gmail.com>
2024-05-12 13:53:14 -0600

    feat(migrations): Agregar migración para crear tabla de usuarios

---
+++ migrations/20240511194458-create-usuarios.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('usuarios', {
+      idUsuario: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombres: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      apellidos: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      correoElectronico: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      contraseña: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('usuarios');
+  }
+};
