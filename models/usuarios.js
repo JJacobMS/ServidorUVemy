3ba6cfd77860d74a0a7b9f3f2356a45405c09986
engaherr <kikgamboa@gmail.com>
2024-05-12 13:52:57 -0600

    feat(models): agregar modelo de usuarios

---
+++ models/usuarios.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class usuarios extends Model {
+    static associate(models) {
+      //usuarios.belongsToMany(models.etiquetas, {through: 'usuariosetiquetas', foreignKey: 'idUsuario'});
+      //usuarios.belongsToMany(models.cursos, {through: 'usuarioscursos', foreignKey: 'idUsuario'});
+      //usuarios.hasMany(models.comentarios, {foreignKey: 'idUsuario'});
+    }
+  }
+  usuarios.init({
+    idUsuario: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    nombres: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    apellidos: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    correoElectronico: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    contraseña: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    freezeTableName: true,
+    modelName: 'usuarios',
+  });
+  return usuarios;
+};
