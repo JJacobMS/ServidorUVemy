3de517accfddd5c62ed9bbfae51451276b01e57a
engaherr <kikgamboa@gmail.com>
2024-05-13 17:35:42 -0600

    feat(config): Agregar archivo de configuración para ClaimTypes

---
+++ config/claimtypes.js
@@ -0,0 +1,6 @@
+const ClaimTypes = {
+    GivenName: 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname',
+    Email: 'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'
+}
+
+module.exports = ClaimTypes;
