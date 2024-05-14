9fa5920c320d4c0508bb831cb0e123dc78cb48db
engaherr <kikgamboa@gmail.com>
2024-05-14 14:43:40 -0600

    fix: arreglar esquema y validador

--- routes/autenticacion.routes.js
+++ routes/autenticacion.routes.js
@@ -1,14 +1,10 @@
 const router = require('express').Router();
 const autenticacion = require('../controllers/autenticacion.controller');
-const { checkSchema, body } = require('express-validator');
-const validadorPeticionesSesion = require('../schemas/autenticacion.validadorpeticion');
+const { checkSchema } = require('express-validator');
+const inicioSesionSchema = require('../schemas/autenticacion.validadorpeticion');
 const validarFormatoPeticion = require('../middlewares/validadorpeticiones.middleware');
 
-router.post('/', body('correoElectronico').notEmpty().trim()
-.isEmail()
-.withMessage('Correo electrónico no válido'), body('contrasena').notEmpty()
-.trim()
-.withMessage('contraseña requerida'), validarFormatoPeticion(), autenticacion.iniciarSesion);
+router.post('/', checkSchema(inicioSesionSchema()),validarFormatoPeticion(), autenticacion.iniciarSesion);
 
 router.get('/tiempo', autenticacion.tiempo);
 
--- schemas/autenticacion.validadorpeticion.js
+++ schemas/autenticacion.validadorpeticion.js
@@ -1,19 +1,12 @@
 const { body } = require('express-validator');
 
-class validadorPeticionesSesion {
-    static inicioSesionSchema() {
-        return [
-            body('correoElectronico')
-                .notEmpty()
-                .trim()
-                .isEmail()
-                .withMessage('Correo electrónico no válido'),
-            body('contrasena')
-                .notEmpty()
-                .trim()
-                .withMessage('contraseña requerida')
-        ];
-    }
+const inicioSesionSchema = () => {
+    console.log("Validando esquema inicio de sesión");
+    return [
+        body("correoElectronico").notEmpty()
+            .trim().isEmail().withMessage("Correo electrónico no válido"),
+        body("contrasena").notEmpty().withMessage("Contraseña requerida")
+    ];
 }
 
-module.exports = { validadorPeticionesSesion } ;
+module.exports = inicioSesionSchema;
