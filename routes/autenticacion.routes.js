100dbb9f4fa426ae0533acf1509ba5b23b9b7365
engaherr <kikgamboa@gmail.com>
2024-05-14 22:25:29 -0600

    fix(routes/schemas): estandarizar nombre de esquema

--- routes/autenticacion.routes.js
+++ routes/autenticacion.routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const autenticacion = require('../controllers/autenticacion.controller');
 const { checkSchema } = require('express-validator');
-const inicioSesionSchema = require('../schemas/autenticacion.validadorpeticion');
+const inicioSesionSchema = require('../schemas/autenticacion.schema');
 const validarFormatoPeticion = require('../middlewares/validadorpeticiones.middleware');
 
 router.post('/', checkSchema(inicioSesionSchema()), validarFormatoPeticion, autenticacion.iniciarSesion);
