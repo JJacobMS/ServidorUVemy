0ba7bbe6cd0b475e5a5a798cfd0ac9c047582cf0
engaherr <kikgamboa@gmail.com>
2024-05-15 21:41:32 -0600

    feat: Agregar registro de usuarios con etiquetas SIN validar arreglo de idsEtiqueta en schema.

--- routes/autenticacion.routes.js
+++ routes/autenticacion.routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const autenticacion = require('../controllers/autenticacion.controller');
 const { checkSchema } = require('express-validator');
-const inicioSesionSchema = require('../schemas/autenticacion.schema');
+const esquemas = require('../schemas/autenticacion.schema');
 const validarFormatoPeticion = require('../middlewares/validadorpeticiones.middleware');
 
-router.post('/', checkSchema(inicioSesionSchema()), validarFormatoPeticion, autenticacion.iniciarSesion);
+router.post('/', checkSchema(esquemas.inicioSesionSchema()), validarFormatoPeticion, autenticacion.iniciarSesion);
+
+router.post('/registro', checkSchema(esquemas.usuarioEsquema()), validarFormatoPeticion, autenticacion.registrarUsuario);
 
 router.get('/tiempo', autenticacion.tiempo);
 
--- schemas/autenticacion.schema.js
+++ schemas/autenticacion.schema.js
@@ -1,4 +1,6 @@
-const inicioSesionSchema = ()  => {
+let self = {};
+
+self.inicioSesionSchema = ()  => {
     return {
         correoElectronico: {
             in: ['body'],
@@ -15,4 +17,52 @@ const inicioSesionSchema = ()  => {
     }
 }
 
-module.exports = inicioSesionSchema;
+self.usuarioEsquema = () => {
+    return {
+        correoElectronico: {
+            in: ['body'],
+            trim: true,
+            isEmail: true,
+            errorMessage: 'Correo electrónico inválido',
+        },
+        contrasena: {
+            in: ['body'],
+            trim: true,
+            notEmpty: true,
+            matches: {
+                options: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+                errorMessage: 'La contraseña debe contener al menos una mayúscula, una minúscula y un número',
+            },
+            errorMessage: 'Contraseña requerida',
+        },
+        nombres: {
+            in: ['body'],
+            trim: true,
+            notEmpty: true,
+            errorMessage: 'Nombres requeridos',
+        },
+        apellidos: {
+            in: ['body'],
+            trim: true,
+            notEmpty: true,
+            errorMessage: 'Apellidos requeridos',
+        },
+        idsEtiqueta: {
+            in: ['body'],
+            isArray: true,
+            optional: true,
+            custom: {
+                options: (value) => value.every((element) => typeof element === 'number'),
+                errorMessage: 'Etiquetas deben ser un arreglo de números',
+            },
+            customSanitizer: {
+                options: (value) => value.map(Number),
+            },
+            errorMessage: 'Etiquetas deben ser un arreglo de números',
+            notEmpty: true,
+            errorMessage: 'Etiquetas requeridas'
+        },
+    }
+}
+
+module.exports = self;
