6b393f1bc50a1aca80d42ed8c54527ae2679682d
sulem <sulem477@gmail.com>
2024-05-23 17:18:49 -0600

    fix: Multer

--- routes/documentos.routes.js
+++ routes/documentos.routes.js
@@ -3,13 +3,14 @@ const documentos = require('../controllers/documentos.controller');
 const autorizacion = require('../middlewares/autorizacion.middleware');
 const { checkSchema } = require('express-validator');
 const validarFormatoPeticion = require('../middlewares/validadorpeticiones.middleware');
+const { subirArchivoPDF } = require("../middlewares/upload.middleware")
 const { crearDocumentoSchema, validarFile, actualizarDocumentoSchema, idDocumentoSchema } = require('../schemas/documento.schema');
 
 //router.get('/', documentos.getAll);
 
 router.get('/clase/:idDocumento', checkSchema(idDocumentoSchema()), validarFormatoPeticion, autorizacion.autorizar(), autorizacion.autorizarIdDocumento("Profesor,Estudiante"), documentos.obtenerArchivoPDF);
 
-router.post('/clase', checkSchema(crearDocumentoSchema()), validarFormatoPeticion, validarFile(), autorizacion.autorizar(), autorizacion.autorizarIdClase("Profesor"), documentos.crear);
+router.post('/clase', subirArchivoPDF.single("file"), checkSchema(crearDocumentoSchema()), validarFormatoPeticion, validarFile(), autorizacion.autorizar(), autorizacion.autorizarIdClase("Profesor"), documentos.crear);
 
 //router.put('/clase/:idDocumento', subirArchivoPDF.single("file"), checkSchema(actualizarDocumentoSchema()), validarFormatoPeticion, validarFile(), documentos.actualizarDocumentoClase);
 
