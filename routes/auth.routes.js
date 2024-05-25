927cbd2b0174499c33e5659268c8cb2268fafc7b
engaherr <kikgamboa@gmail.com>
2024-05-24 23:19:22 -0600

    fix: Actualizar pruebas con richClient y quitar archivos auth desactualizados

--- routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const auth = require('../controllers/auth.controller');
-
-router.post('/', auth.iniciarSesion);
-
-router.get('/tiempo', auth.tiempo);
-
-module.exports = router;
